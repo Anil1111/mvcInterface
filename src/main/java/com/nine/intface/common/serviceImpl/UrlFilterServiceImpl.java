3da68bc14dc94597d8d127b99fe211d973e3af9b
9rubi <1344760467@qq.com>
2018-10-25 16:44:02 +0800

    .

--- src/main/java/com/nine/intface/common/serviceImpl/UrlFilterServiceImpl.java
+++ src/main/java/com/nine/intface/common/serviceImpl/UrlFilterServiceImpl.java
@@ -1,11 +1,14 @@
 package com.nine.intface.common.serviceImpl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.google.common.collect.Ordering;
 import com.nine.intface.common.po.UrlFilter;
 import com.nine.intface.common.dao.UrlFilterMapper;
+import com.nine.intface.common.po.User;
 import com.nine.intface.common.service.IUrlFilterService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -23,11 +26,15 @@ public class UrlFilterServiceImpl extends BaseServiceImpl<UrlFilterMapper, UrlFi
 	 @Override
     public List<UrlFilter> getAllUrlFilter() throws Exception {
         List<UrlFilter> list=list(new QueryWrapper<UrlFilter>().eq("disable_flag",1));
-        Collections.sort(list, (UrlFilter o1,UrlFilter o2)->{
-            int superResult = o1.getParentSort().compareTo(o2.getParentSort());
-            int subResult = o1.getSubSort().compareTo(o2.getSubSort());
-            return superResult != 0 ? superResult : subResult;
-        });
+//        Collections.sort(list, (UrlFilter o1,UrlFilter o2)->{
+//            int superResult = o1.getParentSort().compareTo(o2.getParentSort());
+//            int subResult = o1.getSubSort().compareTo(o2.getSubSort());
+//            return superResult != 0 ? superResult : subResult;
+//        });
+         Ordering<UrlFilter> ordering =Ordering
+                 .from(Comparator.comparing(UrlFilter::getParentSort))
+                 .compound(Comparator.comparing(UrlFilter::getSubSort));
+         Collections.sort(list,ordering);
         return list;
     }
 
